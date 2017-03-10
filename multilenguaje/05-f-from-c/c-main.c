72b333ffeac020b60e3c122fa312742e180a443f
Antonela Taverna <antonela@WTPC>
2017-03-10 17:22:56 -0300

    ejercicios multilenguaje

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,21 @@
-
+//gcc c-main.c f-sum.o -o c-main.e
 #include <stdio.h>
-#include "c-sum.h"
+//#include "c-sum.h" Ahora llamo a la subrutina de fortran.
 
 #define NUM 200
 
+extern void sum_abs_(int *inp, int* num, int* asum);
+
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
+    int data[NUM], num, i, sum;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
 
-    printf("sum=%d\n", sum_abs(data, num));
+    sum_abs_(data, &num, &sum);
+    printf("sum=%d\n", sum);
     return 0;
 }
